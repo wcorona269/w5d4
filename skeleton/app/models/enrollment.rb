b581377f998a2f9b21be4b9a6011290816df51dc
App Academy Student <email@email.com>
2022-03-03 15:06:48 -0500

    finish association exercise

--- skeleton/app/models/enrollment.rb
+++ skeleton/app/models/enrollment.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+
+    belongs_to :courses,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course
+
+    belongs_to :users,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+
 end
